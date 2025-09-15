4abaf1653720ccf6f4a95f202d8ba4e34c5f69ce
Jeferson1709 <jefersonalvessantos62@gmail.com>
2025-09-15 11:04:55 -0300

    feat: Aula de Funções

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+//Parâmetros/argumentos
+//Retorno 
+
+function exibirInfoEstudante(nome,nota) {
+    return `O nome é : ${nome} A nota é: ${nota}`;
+}
+
+console.log(exibirInfoEstudante('Jeferson',7));
+console.log(exibirInfoEstudante('Luiza',9));
+
+//hoisting 
